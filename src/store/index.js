ed4fb3839cb166a0aad996452f5ccdc156102547
eharkins <eli.harkins@gmail.com>
2018-09-11 15:42:58 -0700

    Redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -9,13 +9,30 @@ export default function configureStore(initialState) {
   const middleware = [
     thunk,
     changeURLMiddleware, // eslint-disable-line comma-dangle
-    loggingMiddleware
+    // loggingMiddleware
   ]
-   
-  const composedEnhancers = compose(
-    applyMiddleware(...middleware),
-    window.devToolsExtension ? window.devToolsExtension() : (f) => f
+
+  const actionSanitizer = (action) => (
+    action.type === 'CLONAL_FAMILIES_RECEIVED' && action.availableClonalFamilies ?
+    { ...action, availableClonalFamilies: 'LARGE PAYLOAD' } : action
+  );
+  const stateSanitizer = (state) => {
+    return state.clonalFamilies.availableClonalFamilies ? { ...state, clonalFamilies: {...state.clonalFamilies, availableClonalFamilies:'LARGE PAYLOAD' }} : state;
+  }
+  let composeEnhancers = compose;
+  /* eslint-disable no-underscore-dangle */ 
+  if(process.env.NODE_ENV !== 'production' && typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ){
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Adding these to redux dev tools to handle huge action payload of clonal families
+      actionSanitizer: actionSanitizer,
+      stateSanitizer: stateSanitizer
+    });
+  }
+      
+  const composedEnhancers = composeEnhancers(
+    applyMiddleware(...middleware)
   )
+  /* eslint-enable */
   const store = createStore(rootReducer, initialState, composedEnhancers)
   if (process.env.NODE_ENV !== 'production' && module.hot) {
     console.log("hot reducer reload")
