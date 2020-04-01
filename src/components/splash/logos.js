01bd68ef165da2a0ce8763e650eebb590e375f66
eharkins <eli.harkins@gmail.com>
2020-03-31 18:01:29 -0700

    logos (incl. GH)

--- src/components/splash/logos.js
+++ src/components/splash/logos.js
@@ -5,7 +5,10 @@ export const logos = [
   <a key={1} href="http://www.fredhutch.org/" target="_blank" rel="noreferrer noopener">
     <img alt="logo" width="75" src={require("../../images/fred-hutch-logo-small.png")}/>
   </a>,
+  <a key={2} href="https://github.com/matsengrp/olmsted/" target="_blank" rel="noreferrer noopener">
+    <img alt="logo" width="100" src={require("../../images/github-logo-small.png")}/>
+  </a>,
   <a key={3} href="https://www.nih.gov/" target="_blank" rel="noreferrer noopener">
-    <img alt="logo" width="52" src={require("../../images/nih-logo-small.png")}/>
+    <img alt="logo" width="58" src={require("../../images/nih-logo-small.png")}/>
   </a>,
 ];
