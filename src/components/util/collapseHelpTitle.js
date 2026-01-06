c5231985175b2f0117e0b71d74556f306cbe4fa7
David Rich <david.rich27@gmail.com>
2026-01-06 12:29:59 -0700

    minor help style fix

--- src/components/util/collapseHelpTitle.js
+++ src/components/util/collapseHelpTitle.js
@@ -42,19 +42,7 @@ class CollapseHelpTitle extends React.Component {
           </div>
         }
       >
-        <div
-          style={{
-            marginTop: "10px",
-            marginBottom: "15px",
-            padding: "16px",
-            backgroundColor: "#e7f3ff",
-            border: "1px solid #b3d7ff",
-            borderRadius: "6px",
-            color: "#0a4a7c",
-            fontSize: "14px",
-            lineHeight: "1.5"
-          }}
-        >
+        <div className="info-box">
           {helpText}
         </div>
       </Collapsible>
